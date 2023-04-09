1d670f44f010ed19f9495679a168cd9884af0fcf
NaseemAkram <100962067+NaseemAkram@users.noreply.github.com>
2023-04-09 11:33:17 +0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Model;
+using ProductAPI.Service;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAllProduct")]
+
+        public IEnumerable<Product> GetAllProduct()
+        {
+            return _productService.GetProductList();
+        }
+
+        [HttpGet("{id}")]
+
+        public Product GetProductById(int id)
+        {
+            return _productService.GetProductById(id);
+        }
+
+        [HttpPost("AddProduct")]
+
+        public Product AddProduct(Product product)
+        {
+            return _productService.AddProduct(product);
+        }
+
+        [HttpPut("UpdateProduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return _productService.UpdateProduct(product);
+        }
+
+        [HttpDelete("{id}")]
+        public bool DeleteProduct(int id)
+        {
+            return _productService.DeleteProduct(id);
+        }
+    }
+}
---
+++ Data/ProductDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Model;
+
+namespace ProductAPI.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        private readonly IConfiguration _configration;
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
